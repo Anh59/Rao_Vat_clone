12d73370960216a1a2ebf3927fe3c145217b6b29
Anh59 <trunganhvu59@gmail.com>
2024-06-25 13:26:47 +0700

    vd3

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,14 @@ Route::group(['namespace'=>'Backend','prefix'=>'admin'], function () {
     Route::get('/',[Backend\HomeController::class,'index'])->name('get_admin.home');
     Route::group(['prefix'=>'category'],function(){
         Route::get('/',[Backend\CategoryController::class,'index'])->name('get_admin.category.index');
+        Route::get('create',[Backend\CategoryController::class,'create'])->name('get_admin.category.create');
+        Route::post('create',[Backend\CategoryController::class,'store'])->name('get_admin.category.store');
     });
 });
 
 
 Route::group(['namespace'=>'Frontend'], function () {
     Route::get('',[Frontend\HomeController::class,'index'])->name('get.home');
+    //Route::get('',[Frontend\CategoryController::class,'index'])->name('get.category');
     
 });
